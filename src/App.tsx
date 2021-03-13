5e7f092d0500e1576114747d55d04382ba1079be
SWANos <swanny.aurellia21@gmail.com>
2021-03-13 11:08:06 +0700

    hngpenuhcopas

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,80 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IData {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface IAppState {
+  showTable: boolean;
+}
+
+interface IAppProps {
+  data: IData[]
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+  constructor(props: IAppProps) {
+    super(props);
+    this.state = {
+      showTable: true,
+    }
+  }
+  componentDidMount() {
+    alert("did mount App component !")
+  }
+  componentWillUnmount() {
+    alert("unmounting App component !")
+  }
+  componentDidUpdate() {
+    alert("did update called !")
+  }
+  public toggleShowTable = () => {
+    const { showTable } = this.state;
+    this.setState({showTable: !showTable});
+  }
+  public renderRow = () => {
+    return this.props.data.map((item) => {
+      return (
+        <tr>
+          <td>{item.id}</td>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+        </tr>
+      )
+    })
+  }
+  public renderTable = () => {
+    if (this.state.showTable) {
+      return (
+        <table className="table">
+          <thead>
+            <tr>
+              <td>id</td>
+              <td>name</td>
+              <td>email</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderRow()}
+          </tbody>
+        </table>
+      )
+    }
+  }
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.renderTable()}
+          <button onClick={this.toggleShowTable}>test</button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const dataDummy = [
+  {
+    id: "1",
+    name: "Bambang",
+    email: "bambang@gmail.com"
+  },
+  {
+    id: "2",
+    name: "Budi",
+    email: "budi@yahoo.co.id"
+  },
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={dataDummy}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
